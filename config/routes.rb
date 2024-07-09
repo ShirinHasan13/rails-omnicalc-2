d37f4effdea348469b8eb944a8ede3aa3a7d5acb
ShirinHasan13 <shirin10@chicagobooth.edu>
2024-07-09 08:01:40 +0000

    r

--- config/routes.rb
+++ config/routes.rb
@@ -17,8 +17,8 @@ Rails.application.routes.draw do
   get("/subtract", { :controller => "subtraction", :action => "sub_result" })
   get("/sub_result", { :controller => "subtraction", :action => "subtract_these" })
 
-  get("/multiply", { :controller => "multiplication", :action => "multiplication" })
-  get("/wizard_multiply", { :controller => "multiplication", :action => "multiply" })
+  get("/multiply", { :controller => "multiplication", :action => "multiply_result" })
+  get("/multiply_result", { :controller => "multiplication", :action => "multiply_these" })
 
   get("/divide", { :controller => "division", :action => "show_division_form" })
   get("/wizard_div", { :controller => "div", :action => "divide_these" })
